a90fb412d21f7d4263b5e22719fee6d532a9c17b
h-wei <645500929@qq.com>
2014-12-05 16:52:50 +0800

    [UPDATE] game_code.zip saving target root

--- proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
+++ proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
@@ -1,16 +1,14 @@
 package org.egret.java.HelloEgret;
 
-import android.os.Bundle;
-
 import org.egret.egretframeworknative.EgretRuntimeActivity;
-import org.egret.java.externalInterface.ExternalInterface;
+
+import android.os.Bundle;
 
 public class HelloEgret extends EgretRuntimeActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ExternalInterface.run();
 	}
 
 }
