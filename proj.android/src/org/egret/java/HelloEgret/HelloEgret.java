2fb5a70650b76841717a4a3f37509dfc42a2b8ea
h-wei <645500929@qq.com>
2014-12-11 15:53:40 +0800

    [UPDATE] loader Url
    [ADD] web socket

--- proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
+++ proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
@@ -11,6 +11,17 @@ public class HelloEgret extends EgretRuntimeActivity {
 		super.onCreate(savedInstanceState);
 	}
 
+	/**
+	 * Override your egret game loader url (重写该方法，来指定游戏打包启动的方式)
+	 * - return "game_code.zip" (推荐本地包方式)
+	 * - return "http://www.example.com/game_code.zip" （服务器方式）
+	 * - return "" （调试方式）
+	 */
+	@Override
+	public String getLoaderUrl() {
+		return "";
+	}
+
 }
 
 
