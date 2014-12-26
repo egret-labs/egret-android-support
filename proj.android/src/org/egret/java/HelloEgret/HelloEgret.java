94a924d93c957dcb49d29972c382a5c0bbf20ede
h-wei <645500929@qq.com>
2014-12-26 11:53:00 +0800

    [Ready] to release
    [ADD] load from sd card
    [UPDATE] HelloEgret.java template file

--- proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
+++ proj.android/src/org/egret/java/HelloEgret/HelloEgret.java
@@ -21,16 +21,32 @@ public class HelloEgret extends Activity {
 		super.onCreate(savedInstanceState);
 		egretRoot = new File(getFilesDir(), "egret").getAbsolutePath();
 		gameId = "local";
-		// 1. DEBUG only
-		loaderUrl = "";
-		// 2. local zip
-		// loaderUrl = "launcher.zip";
-		// 3. http request zip
-		// loaderUrl = "http://10.0.2.33/launcher.zip";
+		setLoaderUrl(0);
 		gameEngine = new EgretGameEngine();
 		gameEngine.game_engine_init(this, egretRoot, gameId, loaderUrl);
 		View gameEngineView = gameEngine.game_engine_get_view();
 		setContentView(gameEngineView);
+	}
+	
+	
+	private void setLoaderUrl(int mode) {
+		switch (mode) {
+		case 2:
+			// sd card zip DEBUG mode, use permission WRITE_EXTERNAL_STORAGE
+			// 本地sd包调试模式，需要权限 WRITE_EXTERNAL_STORAGE
+			loaderUrl = "/sdcard/game_code.zip";
+			break;
+		case 1:
+			// http request zip RELEASE mode, use permission INTERNET
+			// 请求网络zip包发布模式，需要权限 INTERNET
+			loaderUrl = "http://www.example.com/game_code.zip";
+			break;
+		default:
+			// local zip RELEASE mode, default mode, `egret publish -compile --runtime native`
+			// 私有空间zip包发布模式, 默认模式, `egret publish -compile --runtime native`
+			loaderUrl = "game_code.zip";
+			break;
+		}
 	}
 	
 }
