2f246108d7482db1088bdda83fb4c63df66a2f67
jackyanjiaqi <jackyanjiaqi@gmail.com>
2016-06-07 15:07:34 +0800

    sorry for the leak of ;

--- proj.android.apus/src/com/egret/gameofpower/apus/Constants.java
+++ proj.android.apus/src/com/egret/gameofpower/apus/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 	public static final String appId = "20173";//Apus提供
 	public static final String appSecret = "9b133fa44d41c7646f7348895256b1ab";//Apus提供
 	public static final String appName = "gameofpower";//游戏名称
-	public static final String notifyUrl = "http://123.57.70.115:8089/"//后台通知地址egret提供
+	public static final String notifyUrl = "http://123.57.70.115:8089/";//后台通知地址egret提供
 }
